52b87486430e2718654b767f438f7e765d8f3a04
TheMolkaPL <themolkapl@gmail.com>
2020-02-27 21:40:02 +0100

    Boss bar configuration

--- src/main/java/pl/craftserve/radiation/BaseConfig.java
+++ src/main/java/pl/craftserve/radiation/BaseConfig.java
@@ -17,11 +17,103 @@
 package pl.craftserve.radiation;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarFlag;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.MemoryConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class BaseConfig {
     private static final char COLOR_CODE = '&';
 
     protected String colorize(String input) {
         return input == null ? null : ChatColor.translateAlternateColorCodes(COLOR_CODE, input);
+    }
+
+    public interface BarConfig {
+        String title();
+        BarColor color();
+        BarStyle style();
+        BarFlag[] flags();
+
+        default BossBar create(Server server) {
+            Objects.requireNonNull(server, "server");
+            String title = Objects.toString(this.title(), "");
+            return server.createBossBar(title, this.color(), this.style(), this.flags());
+        }
+    }
+
+    public static class BarConfigImpl extends BaseConfig implements BarConfig {
+        private final String title;
+        private final BarColor color;
+        private final BarStyle style;
+        private final BarFlag[] flags;
+
+        public BarConfigImpl(ConfigurationSection section) throws InvalidConfigurationException {
+            if (section == null) {
+                section = new MemoryConfiguration();
+            }
+
+            this.title = this.colorize(section.getString("title"));
+
+            String color = section.getString("color", BarColor.WHITE.name());
+            if (color == null) {
+                throw new InvalidConfigurationException("Missing bar color.");
+            }
+
+            try {
+                this.color = BarColor.valueOf(color.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw new InvalidConfigurationException("Unknown bar color: " + color);
+            }
+
+            String style = section.getString("style", BarStyle.SOLID.name());
+            if (style == null) {
+                throw new InvalidConfigurationException("Missing bar style.");
+            }
+
+            try {
+                this.style = BarStyle.valueOf(style.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw new InvalidConfigurationException("Unknown bar style: " + style);
+            }
+
+            List<BarFlag> flags = new ArrayList<>();
+            for (String flagName : section.getStringList("flags")) {
+                try {
+                    flags.add(BarFlag.valueOf(flagName.toUpperCase()));
+                } catch (IllegalArgumentException e) {
+                    throw new InvalidConfigurationException("Unknown bar flag: " + flagName);
+                }
+            }
+            this.flags = flags.toArray(new BarFlag[0]);
+        }
+
+        @Override
+        public String title() {
+            return this.title;
+        }
+
+        @Override
+        public BarColor color() {
+            return this.color;
+        }
+
+        @Override
+        public BarStyle style() {
+            return this.style;
+        }
+
+        @Override
+        public BarFlag[] flags() {
+            return this.flags;
+        }
     }
 }
